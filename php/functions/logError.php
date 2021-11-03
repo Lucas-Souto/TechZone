75d752102c5f4d6fe2b4c4c5a478c005917a007a
Lucas Souto <contato.lucassouto@outlook.com>
2021-11-03 19:33:09 -0300

    Alteração no caminho do log de erros

--- php/functions/logError.php
+++ php/functions/logError.php
@@ -4,7 +4,7 @@ function logError($e, $type)
 	$message = $e->getMessage();
 	$errorSTR = "------------------------- Cought Exception: PDOException({$type}) -------------------------";
 	$errorSTR .= "\nMessage: " . $message . "\nTrace:\n" . $e->getTraceAsString() . "\n\n";
-	$file = fopen(dirname(dirname(__DIR__)) . "/log.txt", "a");
+	$file = fopen(dirname(dirname(__DIR__)) . "/dbError.log", "a");
 
 	fwrite($file, $errorSTR);
 	fclose($file);
